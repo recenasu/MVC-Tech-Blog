aada08983b0d09f09342621f3e50b24ed1f7ec77
Domitius <133902698+recenasu@users.noreply.github.com>
2023-09-25 12:54:40 -0700

    populated package.json file, installed npm modules

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 let sequelize;
 
-// Connect to the appropriate database, remote (JAWSDB_URL) or local
+// Create an instance of the Sequelize class for connecting to the appropriate database, remote (JAWSDB_URL) or local
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
