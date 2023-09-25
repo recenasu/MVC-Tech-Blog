05d9d54fd199eec60d46e4ee071931242f6532db
Domitius <133902698+recenasu@users.noreply.github.com>
2023-09-25 15:16:55 -0700

    added utils directory for helpers and login redirect functions

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+// Export helper functions for use by other files
+module.exports = {
+    format_date: (date) => {
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+        // Format large numbers with commas
+        return parseInt(amount).toLocaleString();
+    },
+};
