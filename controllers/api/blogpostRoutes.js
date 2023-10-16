ec7aefe9c73168bec50fbffc5ca983210928a9f7
Domitius <133902698+recenasu@users.noreply.github.com>
2023-10-16 10:29:22 -0700

    debug of add comment function

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -52,7 +52,7 @@ router.get('/:id', async (req, res) => {
 );
 
 // ***GET single Blogpost data with comment entry template***
-// This route is used to return information on the blogpost selected by the user on the homepage or dashboard.
+// This route is used to return information on the blogpost and the comment entry form.
 router.get('/comment/:id', async (req, res) => {
     try {
         // Get blogpost data
@@ -84,59 +84,25 @@ router.get('/comment/:id', async (req, res) => {
 );
 
 // ***POST the received comment associated with the received blogpost id***
-// This route is used when the user clicks on the Save Comment button after entering a comment on the blogpost comment entry page.
+// This route is used when the user clicks on the Save Comment button after entering a comment on the blogpost comment entry page. It posts the new comment to the database, retrieves all comments from the database, and then returns the blogpost template.
 router.post('/savecomment', async (req, res) => {
+    console.log("made it here!");
     try {
-        const { user_id } = req.session;
+        // const { user_id } = req.session;
         const blog_id = req.body.blog_id;
         const comment_content = req.body.comment;
 
         const payload = {
-            user_id,
+            // **TODO - change user id to just "user_id"
+            user_id: 1,
             comment_content,
             blog_id
         };
 
         const newCommentData = await Comment.create(payload);
         console.log(newCommentData);
+        res.status(200).json(newCommentData);
 
-        // Get blogpost data
-        const blogpostData = await Blogpost.findByPk(blog_id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ]
-        });
-        // Get comment data
-        const commentData = await Comment.findAll({
-            where: {
-                blog_id: payload.blog_id,
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ]
-        });
-
-        console.log(blogpostData);
-        console.log(commentData);
-
-        // Assign the queried data to plain JavaScript objects that do not contain the sequelize properties. 'blogpost' is a single object. 'comment' is an array of objects.
-        const blogpost = blogpostData.get({ plain: true });
-        console.log(blogpost);
-
-        const comment = commentData.map((comment) => comment.get({ plain: true }));
-        console.log(commentData);
-
-        // render the result with the blogpost template
-        res.render('blogpost', {
-            blogpost, comment,
-            // logged_in: req.session ? req.session.logged_in : false
-        });
     } catch (err) {
         res.status(400).json(err);
     }
--- public/js/comment.js
+++ public/js/comment.js
@@ -7,17 +7,29 @@ const saveComment = async () => {
     const payload = {
         comment: document.querySelector('.comment-entry').value,
         blog_id: blogpostId,
+    };
+
+    console.log(payload);
+
+    try {
+        const response = await fetch('/api/savecomment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+
+        });
+
+        if (response.ok) {
+            // refresh the history page
+            window.location.href = `/api/${payload.blog_id}`;
+        } else {
+            console.log("Redirect error.");
         }
-
-        console.log(payload);
-
-        await fetch('/api/savecomment', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-    })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 document.querySelector('#save-comment-button').addEventListener('click', saveComment);
