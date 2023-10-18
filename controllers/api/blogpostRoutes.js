1f72558f269a438df69e1a56af3b60954f37178e
Domitius <133902698+recenasu@users.noreply.github.com>
2023-10-17 23:09:49 -0700

    added new post form template and route

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -5,7 +5,6 @@ const withAuth = require('../../utils/auth');
 // ***GET single Blogpost data***
 // This route is used to return information on the blogpost selected by the user on the homepage or dashboard.
 router.get('/:id', async (req, res) => {
-    console.info("made it here");
     try {
         // Get blogpost data
         const blogpostData = await Blogpost.findByPk(req.params.id, {
@@ -96,7 +95,6 @@ router.get('/comment/:id', async (req, res) => {
 // ***POST the received comment associated with the received blogpost id***
 // This route is used when the user clicks on the Save Comment button after entering a comment on the blogpost comment entry page. It posts the new comment to the database, retrieves all comments from the database, and then returns the blogpost template.
 router.post('/savecomment', async (req, res) => {
-    console.log("made it here!");
     try {
         const { user_id } = req.session;
         const blog_id = req.body.blog_id;
@@ -119,6 +117,45 @@ router.post('/savecomment', async (req, res) => {
 
 // ***GET route that returns the newPost page***
 // This route is executed when the user clicks on the New Post button on the Dashboard. It returns the newPost template for the user to add a new post.
+router.get('/newpost/form', async (req, res) => {
+    try {
+        console.log('made it here!');
+        const randomNum = Math.random();
+        let postEmoji;
+        
+        // set emoji variable to go with new post.
+        if (randomNum > 0.9) {
+        postEmoji = '&#128640;'
+        } else if (randomNum > 0.8) {
+            postEmoji = '&#128507;'
+        } else if (randomNum > 0.7) {
+            postEmoji = '&#128508;'
+        } else if (randomNum > 0.6) {
+            postEmoji = '&#128509;'
+        } else if (randomNum > 0.5) {
+            postEmoji = '&#128511;'
+        } else if (randomNum > 0.4) {
+            postEmoji = '&#128640;'
+        } else if (randomNum > 0.3) {
+            postEmoji = '&#128642;'
+        } else if (randomNum > 0.2) {
+            postEmoji = '&#128692;'
+        } else if (randomNum > 0.1) {
+            postEmoji = '&#128761;'
+        } else if (randomNum > 0.0) {
+            postEmoji = '&#129380;'
+        }; 
+        
+        // render the result with the homepage template
+        res.render('newPost', {
+            postEmoji, session: req.session,
+            logged_in: req.session ? req.session.logged_in : false
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
 
 // ***POST route for posting a new blogpost***
 // This route is executed when the user clicks on the Save Post button on the newPost page. It adds the user's post to the blogpost model tagged with the user's user_id.
---
+++ public/js/post.js
@@ -0,0 +1,36 @@
+// Listens for button clicks on the individual blogpost page. If the Add Comment button is clicked, sends a get route to the server for the Add Comment template.
+
+// ***TODO** turn this into a post for the Save Post button
+const saveComment = async () => {
+
+    const blogpostId = document.querySelector('.save-button-row-container').getAttribute('data-blogpost-id');
+
+    const payload = {
+        comment: document.querySelector('.comment-entry').value,
+        blog_id: blogpostId,
+    };
+
+    console.log(payload);
+
+    try {
+        const response = await fetch('/api/savecomment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+
+        });
+
+        if (response.ok) {
+            // refresh the history page
+            window.location.href = `/api/${payload.blog_id}`;
+        } else {
+            console.log("Redirect error.");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+document.querySelector('#save-comment-button').addEventListener('click', saveComment);
